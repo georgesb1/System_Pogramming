643aae968c6a42fc2a43f3227ea4065abddc69cf
enzotatsi <enzo.nwedjiwe@2025.ucac-icam.com>
2022-11-30 15:57:32 +0100

    vue

--- ProjetResto/ProjetResto/Modèle/Salle/serveur.cs
+++ ProjetResto/ProjetResto/Modèle/Salle/serveur.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SpriteLibrary;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,16 @@ namespace ProjetResto.Modèle
 {
     class Serveur
     {
+        public int PosX { get; set; }
+
+        public int PosY { get; set; }
+
+        public Sprite Sprite { get; set; }
+
+        public Serveur(int posX, int posY)
+        {
+            this.PosX = posX;
+            this.PosY = posY;
+        }
     }
 }
---
+++ ProjetResto/ProjetResto/Vue/Form3.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ProjetResto
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textNom = new System.Windows.Forms.TextBox();
+            this.textContact = new System.Windows.Forms.TextBox();
+            this.listPlace = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(65, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nom et Prénom";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(65, 209);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 21);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Contact";
+            this.label6.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(65, 287);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(159, 21);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Nombres de places ";
+            this.label7.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textNom
+            // 
+            this.textNom.Location = new System.Drawing.Point(267, 120);
+            this.textNom.Name = "textNom";
+            this.textNom.Size = new System.Drawing.Size(202, 23);
+            this.textNom.TabIndex = 1;
+            // 
+            // textContact
+            // 
+            this.textContact.Location = new System.Drawing.Point(267, 207);
+            this.textContact.Name = "textContact";
+            this.textContact.Size = new System.Drawing.Size(202, 23);
+            this.textContact.TabIndex = 1;
+            this.textContact.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // listPlace
+            // 
+            this.listPlace.FormattingEnabled = true;
+            this.listPlace.ItemHeight = 15;
+            this.listPlace.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.listPlace.Location = new System.Drawing.Point(267, 287);
+            this.listPlace.Name = "listPlace";
+            this.listPlace.Size = new System.Drawing.Size(202, 19);
+            this.listPlace.TabIndex = 2;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 431);
+            this.Controls.Add(this.listPlace);
+            this.Controls.Add(this.textNom);
+            this.Controls.Add(this.textContact);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label1);
+            this.Name = "Form3";
+            this.Text = "Panneau de réservation";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textNom;
+        private System.Windows.Forms.TextBox textContact;
+        private System.Windows.Forms.ListBox listPlace;
+    }
+}
